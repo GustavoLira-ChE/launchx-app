7cb88fb1c573ce082aa0e074deab8e5f7f96b765
Gustavo Lira <gu.lirar23@gmail.com>
2022-04-21 12:17:34 -0500

    add: index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+console.log(carlo.name)
+const rodrigo = new MissionCommander("rodrigo")
+console.log(rodrigo.name)
+const fernanda = new MissionCommander("fernanda")
+console.log(fernanda.name)
